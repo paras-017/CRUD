6da99e751e748c635a335d1ae447da8e32c0731e
paras-017 <paraschauhan0017@gmail.com>
2023-10-07 14:51:52 +0530

    update note funcitonality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
 function App() {
   const [notes, setNotes] = useState(null)
   const [createForm, setCreateForm] = useState({title:"", description:""})
+  const [updateForm, setUpdateForm] = useState({_id:null,title:"", description:""})
   const fetchAllNotes = async ()=>{ 
    const res = await axios.get('http://localhost:3000')
    setNotes(res.data.notes)
@@ -25,6 +26,13 @@ function App() {
       [name]:value
     })
   }
+  const handleUpdateFieldChange = (e) =>{
+    const {name, value} = e.target 
+    setUpdateForm({
+      ...updateForm,
+      [name]:value
+    })
+  }
   const createNote = async (e)=>{
     e.preventDefault()
     //create a note
@@ -32,6 +40,27 @@ function App() {
    console.log(res)
    setNotes([...notes, res.data.note])
    setCreateForm({title:"", description:""})
+  }
+  const updateNote = async (e)=>{
+    e.preventDefault()
+    const {title, description} = updateForm
+    const res = await axios.put(`http://localhost:3000/${updateForm._id}`,{title, description})
+    //update Notes
+    const newNotes = [...notes]
+    //finding index of update note
+    const noteIndex = notes.findIndex(note=>{
+      return note._id === updateForm._id
+    })
+    //updating note array
+    newNotes[noteIndex] = res.data.note
+    setNotes(newNotes)
+    //clear  update form state
+    setUpdateForm({_id:null,title:'',body:''})
+
+  }
+  const toggleUpdate = async (note)=>{
+    // get the current note value
+    setUpdateForm({_id:note._id,title:note.title, description:note.description})
   }
   useEffect(() => {
     fetchAllNotes()
@@ -52,16 +81,29 @@ function App() {
     )
    })}
 
+  {/* Update Form for note */}
+  {updateForm._id &&
+  <div>
+    <h1>Update a form</h1>
+    <form onSubmit={updateNote}>
+      <input onChange={handleUpdateFieldChange} type="text" name='title' value={updateForm.title}/>
+      <textarea onChange={handleUpdateFieldChange} type="text" name='description' value={updateForm.description}/>
+      <button type="submit">Update Note</button>
+    </form>
+  </div>
+  }
+
 
    {/* Create a note Form */}
+   {!updateForm._id &&
    <div>
-    <h1>Create a Note Form</h1>
-    <form onSubmit={createNote}>
-      <input type="text" name='title' value={createForm.title} onChange={updateCreateFormField}/>
-      <textarea type="text" name='description' value={createForm.description} onChange={updateCreateFormField}/>
-      <button type='submit'>Submit</button>
-    </form>
-   </div>
+   <h1>Create a Note Form</h1>
+   <form onSubmit={createNote}>
+     <input type="text" name='title' value={createForm.title} onChange={updateCreateFormField}/>
+     <textarea type="text" name='description' value={createForm.description} onChange={updateCreateFormField}/>
+     <button type='submit'>Submit</button>
+   </form>
+  </div>}
    </>
   )
 }
