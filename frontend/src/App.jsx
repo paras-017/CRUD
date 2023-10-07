66598246daef98b59d73d3341def85fc3e114c70
paras-017 <paraschauhan0017@gmail.com>
2023-10-07 12:28:49 +0530

    Notes fetched

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import  axios  from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [notes, setNotes] = useState(null)
+  const fetchAllNotes = async ()=>{
+    
+   const res = await axios.get('http://localhost:3000')
+   setNotes(res.data.notes)
+  }
+  useEffect(() => {
+    fetchAllNotes()
+  }, [])
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <>
+   <h1>Notes</h1>
+   {/* DISPLAY NOTES */}
+   {notes && notes.map((note)=>{
+    return (
+      <div key={note._id}>
+        <h1>Title: {note.title}</h1>
+        <h1>Description: {note.description}</h1>
+        <button onClick={()=>deleteNote(note._id)}>Delete</button>
+        <button onClick={()=>toggleUpdate(note)}>Edit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
+   })}
+   </>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const connectToDb = require('./database/mongoDbconnect')
 const router = express.Router()
 const app = express()
 const port = process.env.PORT || 8000
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 connectToDb()
 app.post('/', createNote)
